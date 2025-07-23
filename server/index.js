ce5e6e05725c6b12f0526a8f2562e17ef8e50266
Berk Zengin <118611658+Berrrk@users.noreply.github.com>
2025-07-24 00:38:29 +0300

    Backend update

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hey, backend working!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is working: http://localhost:${PORT}`);
+});
